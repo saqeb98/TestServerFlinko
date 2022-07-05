742c0bc1c98b967f77bc93db5a500a4970cd2bc6
saqeb98 <76609145+saqeb98@users.noreply.github.com>
2022-07-05 16:12:07 +0530

    Update fire-flink.py

--- fire-flink.py
+++ fire-flink.py
@@ -3,42 +3,36 @@ from aifc import Error
 
 import requests
 import json
+import sys
 
-class Execution_Failure(Error):
+
+class Test_Failed(Error):
     pass
 
-def login(mail, password):
+token = sys.argv[1]
+
+def login(token):
     s = requests.Session()
-    payload = {
-        'emailId': mail,
-        'password': password,
-    }
-    logurl = 'https://app.flinko.com:8101/optimize/v1/public/user/signin'
     head = {
         "accept": "application/json",
         "Content-Type": "application/json"
     }
-    resp = requests.post(logurl, json=payload)
-    tk = json.loads(resp.content)
-    token = tk['responseObject']['access_token']
     head["Authorization"] = "Bearer " + token
-
-    suiteid = 'SUITE1015'
-    pes = s.post('http://10.10.10.172:8109/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head)
+    suiteid = 'SUITE1016'
+    pes = s.post('https://app.flinko.com:8109/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head)
     out = json.loads(pes.content)
     exid = out['responseObject']['id']
 
-    time.sleep(2)
+    time.sleep(3)
     sc = 0
-    fr1 = 0
     while (sc < 1):
-        r1 = s.get('http://10.10.10.172:8109/optimize/v1/dashboard/execution/' + exid, headers=head)
+        r1 = s.get('https://app.flinko.com:8109/optimize/v1/dashboard/execution/' + exid, headers=head)
         c1 = json.loads(r1.content)
         fr1 = c1['responseObject']['executionStatus']
         print('status : ' + fr1 + '......')
         if (fr1 == "Completed" or fr1 == "Terminated" or fr1 == "Warning"):
             if fr1 == 'Completed':
-                r2 = s.get('http://10.10.10.172:8110/optimize/v1/executionResponse/result/' + exid, headers=head)
+                r2 = s.get('https://app.flinko.com:8110/optimize/v1/executionResponse/result/' + exid, headers=head)
                 c2 = json.loads(r2.content)
                 fr2 = c2['responseObject']['suiteStatus']
                 if fr2 == 'FAIL':
@@ -50,4 +44,5 @@ def login(mail, password):
                 print("End Result : " + fr1)
                 sc = 1
         time.sleep(10)
-login('mohammed.saqeb@testyantra.com', 'Password@123')
+
+login(token)
